3468c4f07e7190dc2225b37b13444bc961f01ceb
salamantalel <nmoinkett@gmail.com>
2025-06-16 15:37:15 +0300

    complete loops assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// writeCards function
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+// countDown function
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+module.exports = { writeCards,countDown};
